9ecc57f9acb0e9c52a8c1aa2d27956a953fb3b15
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-03-27 08:47:29 +0200

    Initial Commit

---
+++ LoginView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginView
+//
+//  Created by Manyuchi, Carrington C on 2025/03/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by Manyuchi, Carrington C on 2025/03/27.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
